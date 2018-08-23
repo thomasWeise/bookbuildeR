b11a1d445a97b6c7119075fe91918e3b6af2107b
Thomas Weise <tweise@hfuu.edu.cn>
2018-08-24 06:43:27 +0800

    Fixed Documentation Error for Procedure `exit`

--- R/logger.R
+++ R/logger.R
@@ -2,7 +2,7 @@
 .logger <- makeLogger(TRUE);
 
 #' @title Quit the Processing with an Error
-#' @describeIn Well, exit the current process, throw an error.
+#' @description Well, exit the current process, throw an error.
 #' @param ... the error message
 #' @export exit
 exit <- function(...) {
