b389737104f13d3225751ab82f4b7e9f137dddde
Thomas Weise <tweise@hfuu.edu.cn>
2018-08-24 10:39:04 +0800

    Better pandoc Support, also supporting cabal

--- R/meta.R
+++ R/meta.R
@@ -1,32 +1,46 @@
 
 #' @title Get the Commit Identifier from the Environment
-#' @description This function will only work if we build with travis.ci. It
-#'   loads the commit identifier from the environment.
+#' @description This function will only work if we build with travis.ci or have
+#'   an environment variable \code{COMMIT} set. It loads the commit identifier
+#'   from the environment.
 #' @return a string identifying the commit of the repository for which we build
 #' @export meta.commit
 meta.commit <- function() {
   env.var <- "TRAVIS_COMMIT";
   commit <- Sys.getenv(x=env.var, unset=NA);
   if(is.null(commit) || is.na(commit)|| (length(commit) != 1L) || (nchar(commit) <= 0L)) {
-    exit("Commit unknown because environment variable '",
-         env.var,
-         "' not set, maybe you are not building with travis.ci?");
+    env.var.2 <- "COMMIT";
+    commit <- Sys.getenv(x=env.var.2, unset=NA);
+    if(is.null(commit) || is.na(commit)|| (length(commit) != 1L) || (nchar(commit) <= 0L)) {
+      exit("Commit unknown because environment variables '",
+           env.var,
+           "' and '",
+           env.var.2,
+           "' are both not set. Set one of them by hand or build with travis.ci.");
+    }
   }
   return(commit);
 }
 
 #' @title Get the Repository Identifier from the Environment
-#' @description This function will only work if we build with travis.ci. It
-#'   loads the repository identifier from the environment.
+#' @description This function will only work if we build with travis.ci or have
+#'   an environment variable \code{REPOSITORY} set. It loads the repository
+#'   identifier from the environment.
 #' @return a string identifying the repository for which we build
 #' @export meta.repository
 meta.repository <- function() {
   env.var <- "TRAVIS_REPO_SLUG";
   repo    <- Sys.getenv(x=env.var, unset=NA);
   if(is.null(repo) || is.na(repo) || (length(repo) != 1L) || (nchar(repo) <= 0L)) {
-    exit("Repository unknown because environment variable '",
-         env.var,
-         "' not set, maybe you are not building with travis.ci?");
+    env.var.2 <- "REPOSITORY";
+    repo <- Sys.getenv(x=env.var.2, unset=NA);
+    if(is.null(repo) || is.na(repo)|| (length(repo) != 1L) || (nchar(repo) <= 0L)) {
+      exit("repository unknown because environment variables '",
+           env.var,
+           "' and '",
+           env.var.2,
+           "' are both not set. Set one of them by hand or build with travis.ci.");
+    }
   }
   return(repo);
 }
--- R/pandoc.R
+++ R/pandoc.R
@@ -2,10 +2,29 @@
 #' @description Invoke Pandoc with the provided parameters.
 #' @param sourceFile the path to the source file
 #' @param destFile the path to the destination file
+#' @param format.in the input format
+#' @param format.out the output format
+#' @param standalone should we produce a stand-alone document?
+#' @param tabstops the number of spaces with which we replace a tab character,
+#'   or \code{NA} to not replace
+#' @param toc.print print a table of contents?
+#' @param toc.depth the depth of the table of content to print
+#' @param crossref use pandoc-crossref?
+#' @param bibliography do we have a bibliography?
 #' @param ... the arguments to be passed to pandoc
 #' @return the canonical path to the destination file
 #' @include logger.R
-invoke.pandoc <- function(sourceFile, destFile, ...) {
+#' @export pandoc.invoke
+pandoc.invoke <- function(sourceFile, destFile,
+                          format.in="markdown",
+                          format.out="latex",
+                          standalone=TRUE,
+                          tabstops=2L,
+                          toc.print=TRUE,
+                          toc.depth=3L,
+                          crossref=TRUE,
+                          bibliography=TRUE,
+                          ...) {
   # get the canonical path of the source file
   sourceFile <- normalizePath(sourceFile, mustWork=TRUE);
   if(!file.exists(sourceFile)) {
@@ -29,20 +48,65 @@ invoke.pandoc <- function(sourceFile, destFile, ...) {
   destDir  <- normalizePath(destDir, mustWork = TRUE);
   destFile <- normalizePath(file.path(destDir, basename(destFile)), mustWork = FALSE);
 
+
+  # find the location of pandoc
+  pandoc <- "pandoc";
+  env <- character();
+  home <- Sys.getenv(x="HOME", unset=NA);
+  if((!(is.na(home) || is.null(home))) && (length(home) == 1L) && (nchar(home) > 0L)) {
+    # we test whether it has been installed via cabal
+    test <- normalizePath(file.path(home, ".cabal", "bin", "pandoc"), mustWork = FALSE);
+    if(file.exists(test)) {
+      pandoc <- test;
+      env.path <- Sys.getenv(x="PATH", unset=NA);
+      if((!(is.na(home) || is.null(home))) && (length(home) == 1L) && (nchar(home) > 0L)) {
+        # if so, we need to add the cabal binaries folder to the path
+        env <- c(paste("PATH=", env.path, ":", dirname(pandoc), ";", sep="", collapse=""));
+      }
+      .logger("Using pandoc executable '", pandoc, "' from cabal.");
+    }
+  }
+
   .logger("Applying pandoc to create '", destFile, "' from '", sourceFile, "'.");
 
   wd <- getwd();
   setwd(sourceDir);
-  args <- as.vector(unname(unlist(list(..., sourceFile, "-o", destFile), recursive = TRUE)));
+  args <- c(paste("--read=", format.in, sep="", collapse=""),
+            paste("--write=", format.out, sep="", collapse=""),
+            paste("--output=", destFile, sep="", collapse=""));
+  if(!is.na(tabstops)) {
+    args <- c(args, paste("--tab-stop=", tabstops, sep="", collapse=""));
+  }
+  if(standalone) {
+    args <- c(args, "--standalone");
+  }
+  if(toc.print) {
+    args <- c(args, "--table-of-contents");
+    if(!is.na(toc.depth)) {
+      args <- c(args, paste("--toc-depth=", toc.depth, sep="", collapse=""));
+    }
+  }
 
-  result <- system2("pandoc", args=args);
+  if((!(is.null(crossref) || is.na(crossref))) && crossref) {
+    args <- c(args, "--filter pandoc-crossref");
+  }
+
+  if((!(is.null(bibliography) || is.na(bibliography))) && bibliography) {
+    args <- c(args, "--filter pandoc-citeproc");
+  }
+
+  args <- c(args, list(...));
+  args <- c(args, sourceFile);
+  args <- as.vector(unname(unlist(args, recursive = TRUE)));
+
+  result <- system2(pandoc, args=args, env=env);
 
   if(result != 0L) {
-    exit("pandoc has failed with error code ", result, " for arguments '",
+    exit(pandoc, " has failed with error code ", result, " for arguments '",
          paste(args, sep=" ", collapse=" "), "' in directory '", sourceDir, "'.");
   }
 
-  .logger("pandoc has succeeded for arguments '",
+  .logger(pandoc, " has succeeded for arguments '",
          paste(args, sep=" ", collapse=" "), "' in directory '", sourceDir, "'.");
   setwd(wd);
 }
